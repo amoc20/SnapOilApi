b0000bf1cf36e114a44e3991ebd106d56009267d
amoc20 <moc.ales@seznam.cz>
2023-03-29 13:02:58 +0200

    Database connection

---
+++ WebAPI/GasStation.cs
@@ -0,0 +1,10 @@
+﻿namespace WebAPI;
+
+public class GasStation
+{
+	public int StationId { get; set; }
+	public string City { get; set;}
+	public string Address { get; set;}
+	public string Brand { get; set;}
+	public int BrandId { get; set; }
+}
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,13 +1,28 @@
 using System.Drawing;
 using ImageProcessing;
+using MySqlConnector;
 using WebAPI;
 
 var formFiller = new FormFiller();
 var imageProcessor = new ImageProcessor();
 
+// Database connection
+const string server = "localhost";
+const string username = "snap_oil";
+const string password = "123";
+const string database = "snap_oil";
+const string connString = $"Server={server};User ID={username};Password={password};Database={database}";
+
+using var connection = new MySqlConnection(connString);
+Console.WriteLine("Connecting to db...");
+await connection.OpenAsync();
+Console.WriteLine("Connected to db");
+
+// Build
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+// Image processing
 app.MapPost("/uploadimg", async (IFormFile file) =>
 {
 	(double natural95, double diesel)? prices = null;
@@ -16,17 +31,77 @@ app.MapPost("/uploadimg", async (IFormFile file) =>
 	{
 		await file.CopyToAsync(memoryStream);
 		using var img = Image.FromStream(memoryStream);
-		string stationName = file.FileName.Split(".").First();
-		prices = imageProcessor.GetPrices(img, stationName);
+		string brandName = file.FileName.Split(".").First();
+		prices = imageProcessor.GetPrices(img, brandName);
 	}
 
 	return prices is null ? Results.NotFound() : Results.Ok(prices);
 });
 
+// Prices upload
 app.MapPost("/uploadprices", (GasStationPrices prices) =>
 {
 	formFiller.SendInfo(prices);
 	return Results.Ok(prices);
+});
+
+// Stations get
+app.MapGet("/stations", async () =>
+{
+	using var command = new MySqlCommand("" +
+		$"SELECT id_station, city, address, brand_name, id_brand FROM stations NATURAL JOIN brands;",
+		connection);
+	using var reader = await command.ExecuteReaderAsync();
+
+	var output = new List<GasStation>();
+	while (reader.Read())
+	{
+		var stationId = reader.GetInt32(0)!;
+		var city = reader.GetString(1)!;
+		var address = reader.GetString(2)!;
+		var brandName = reader.GetString(3)!;
+		var brandId = reader.GetInt32(4)!;
+
+		output.Add(new GasStation
+		{
+			StationId = stationId,
+			City = city,
+			Address = address,
+			Brand = brandName,
+			BrandId = brandId
+		});
+	}
+
+	return output;
+});
+
+app.MapGet("/stations/{brand:int}", async (string brand) =>
+{
+	using var command = new MySqlCommand("" +
+		$"SELECT id_station, city, address, brand_name, id_brand FROM stations NATURAL JOIN brands WHERE id_brand = {brand};",
+		connection);
+	using var reader = await command.ExecuteReaderAsync();
+
+	var output = new List<GasStation>();
+	while (reader.Read())
+	{
+		var stationId = reader.GetInt32(0)!;
+		var city = reader.GetString(1)!;
+		var address = reader.GetString(2)!;
+		var brandName = reader.GetString(3)!;
+		var brandId = reader.GetInt32(4)!;
+
+		output.Add(new GasStation
+		{
+			StationId = stationId,
+			City = city,
+			Address = address,
+			Brand = brandName,
+			BrandId = brandId
+		});
+	}
+
+	return output;
 });
 
 app.Run();
